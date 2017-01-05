257dd9a872cea3aa6a51936749f51baab245017f
wael2000 <waelibrahim2000@hotmail.com>
2017-01-06 01:31:30 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <h1> Get username:password from Secret </h1>
 <div>
 <?php
-echo($_ENV["USERNAME"]);
-echo(":");
-echo($_ENV["PASSWORD"]);
+echo $_ENV["USERNAME"];
+echo ":";
+echo $_ENV["PASSWORD"];
 ?>
 </div>
 <h1> Get country:language from ConfigMap </h1>
 <div>
 <?php
-echo($_ENV["COUNTRY"]);
-echo(":");
-echo($_ENV["LANGUAGE"]);
+echo $_ENV["COUNTRY"];
+echo ":";
+echo $_ENV["LANGUAGE"];
 ?>
 </div>
 
