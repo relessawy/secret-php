d68f5ef1ffe281f942e2db9be0fe0849c36dc5cf
wael2000 <waelibrahim2000@hotmail.com>
2017-01-06 00:32:57 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<h1> Get username:password from Secret </h1>
+<div>
+<?php
+echo($_ENV["USERNAME"]);
+echo(":");
+echo($_ENV["PASSWORD"]);
+?>
+</div>
+<h1> Get country:language from ConfigMap </h1>
+<div>
+<?php
+echo($_ENV["COUNTRY"]);
+echo(":");
+echo($_ENV["LANGUAGE"]);
+?>
+</div>
+
